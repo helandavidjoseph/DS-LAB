4e9da54ab1759f63f7c418f28632c81e7ba220f3
helandavidjoseph <helandavidjoseph466@gmail.com>
2026-08-31 11:54:16 +0530

    Add files via upload

---
+++ h3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+int data;
+struct node*next;
+};
+int main()
+{
+struct node*head=NULL,*newnode,*temp;
+int n,i;
+printf("Enter the number of nodes:");
+scanf("%d",&n);
+head=NULL;
+for(i=0;i<n;i++)
+{
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter data:");
+scanf("%d",&newnode->data);
+newnode->next=NULL;
+if(head==NULL)
+{
+head=newnode;
+temp=newnode;
+}
+else
+{
+temp->next=newnode;
+temp=newnode;
+}
+}
+printf("Linked List:");
+temp=head;
+while(temp!=NULL)
+{
+printf("%d->",temp->data);
+temp=temp->next;
+}
+printf("NULL");
+return 0;
+}
