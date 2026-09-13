74a2b91b1bb073601198620376a1eb570dbcca64
helandavidjoseph <helandavidjoseph466@gmail.com>
2026-09-13 21:53:31 +0530

    Create Last repeated character.c

---
+++ Last repeated character.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    char a[100];
+    int n,i,j;
+    printf("Enter the size:");
+    scanf("%d",&n);
+    printf("Enter the characters:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%c",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        for(j=i-1;j>=0;j--)
+        {
+            if(a[i]==a[j])
+            {
+                printf("Last repeated character=%c",a[i]);
+                return 0;
+            }
+        }
+    }
+    printf("No repeated character");
+    return 0;
+    
+}
