05a0f32726f10060e1e1c82be8bb4f31bcdfdeb2
helandavidjoseph <helandavidjoseph466@gmail.com>
2026-09-14 12:18:43 +0530

    Update Binary search 3D array.c

--- Binary search 3D array.c
+++ Binary search 3D array.c
@@ -9,7 +9,7 @@ int main()
     scanf("%d %d %d", &x, &y, &z);
     printf("Enter sorted elements:\n");
     for(i = 0; i < x; i++)
-    
+    {
         for(j = 0; j < y; j++)
         {
             for(k = 0; k < z; k++)
